562ba4d7e03860f73222aa12b6d382b7350c241f
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-23 16:14:38 -0700

    Set up endpoints for http requests

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const mysql = require('mysql');
+const mysqlConfig = require('./mysql/mysql.config.js');
+const assert = require('assert');
+
+let db;
+MongoClient.connect('mongodb://localhost:27017/DL', (err, database) => {
+  assert.equal(null, err);
+  db = database;
+  console.log('Connected successfully to server');
+});
+
+const connection = mysql.createConnection(mysqlConfig);
+connection.connect();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/:productId', (req, res) => {
+  const collection = db.collection('page_views');
+  collection.insert({
+    product_id: req.params.productId,
+    user_id: req.query.user_id,
+  }, () => {
+    res.end();
+  });
+});
+
+app.post('/products', (req, res) => {
+  connection.query('INSERT INTO products SET ?', req.body, () => {
+    res.end();
+  });
+});
+
+app.post('/signup', (req, res) => {
+  connection.query('INSERT INTO users SET ?', req.body, () => {
+    res.end();
+  });
+});
+
+app.post('/purchase', (req, res) => {
+  // Save Shopping cart
+  connection.query('INSERT INTO shopping_carts SET ?', {
+    user_id: req.body.user_id,
+    subtotal: req.body.subtotal,
+  }, (err, results) => {
+    // Save Purchase
+    connection.query('INSERT INTO purchases SET ?', {
+      user_id: req.body.user_id,
+      shopping_cart_id: results.insertId,
+    }, () => {
+      // Save reviews
+      let savedReviews = 0;
+      // Save products and shopping_carts join table
+      let savedProductsShoppingCarts = 0;
+
+      req.body.products.forEach((product) => {
+        connection.query('INSERT INTO reviews SET ?', {
+          user_id: req.body.user_id,
+          product_id: product.id,
+          title: product.review_title,
+          review: product.review_body,
+          rating: product.rating,
+        }, () => {
+          savedReviews += 1;
+          if (savedReviews === req.body.products.length &&
+          savedProductsShoppingCarts === req.body.products.length) {
+            res.end();
+          }
+        });
+      });
+      connection.query('INSERT INTO products_shopping_carts SET ?', {
+        user_id: req.body.user_id,
+        shopping_cart_id: results.insertId,
+      }, () => {
+        savedProductsShoppingCarts += 1;
+        if (savedProductsShoppingCarts === req.body.products.length &&
+        savedReviews === req.body.products.length) {
+          res.end();
+        }
+      });
+    });
+  });
+});
+
+app.post('/mouseovers', (req, res) => {
+  const collection = db.collection('mouseovers');
+  collection.insert({
+    mouseovers: req.body,
+  }, () => {
+    res.end();
+  });
+});
+
+app.listen(3000); // Listening on port 3000
---
+++ mysql/schme.sql
@@ -0,0 +1,78 @@
+/* Execute this file by using: mysql -u <USER> -p < schema.sql */
+USE dl;
+
+DROP TABLE IF EXISTS purchases;
+DROP TABLE IF EXISTS shopping_carts;
+DROP TABLE IF EXISTS products_shopping_carts;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(64),
+  last_name VARCHAR(64),
+  age TINYINT unsigned,
+  email VARCHAR(255),
+  password VARCHAR(255),
+  street_address VARCHAR(128),
+  zip_code VARCHAR(32),
+  city VARCHAR(64),
+  state VARCHAR(64),
+  gender VARCHAR(32),
+  marrital_status VARCHAR(16),
+  children TINYINT unsigned,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  price DECIMAL(16, 2),
+  user_id BIGINT unsigned NOT NULL,
+  category VARCHAR(255),
+  ratings_avg TINYINT unsigned,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE reviews (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  user_id BIGINT unsigned NOT NULL,
+  product_id BIGINT unsigned NOT NULL,
+  rating TINYINT unsigned,
+  title VARCHAR(255),
+  review VARCHAR(4095),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (product_id) REFERENCES products (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products_shopping_carts (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  price DECIMAL(16, 2),
+  user_id BIGINT unsigned NOT NULL,
+  category VARCHAR(255),
+  ratings_avg TINYINT unsigned,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE shopping_carts (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  user_id BIGINT unsigned NOT NULL,
+  subtotal DECIMAL(16, 2),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE purchases (
+  id BIGINT unsigned NOT NULL AUTO_INCREMENT,
+  user_id BIGINT unsigned NOT NULL,
+  shopping_cart_id BIGINT unsigned NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (shopping_cart_id) REFERENCES shopping_carts (id),
+  PRIMARY KEY (id)
+);
